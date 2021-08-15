e0871a86bc7620f8eeb918e7e75a999113b71688
aaron <aaron.huang@ridemoovo.com>
2021-08-15 17:57:45 +0800

    feat(root index.ts): change to  apollo-express & upgrade to v3

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import 'reflect-metadata';
-import { ApolloServer } from 'apollo-server';
+// import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
 
 import BookResolver from '@/resolver/book';
 import AuthorResolver from '@/resolver/author';
+const PORT = 4000;
 
 async function bootstrap() {
-  try {
-    const dbConnection = await createConnection();
-  } catch (e) {
-    console.error(e);
-  }
+  const dbConnection = await createConnection();
 
   const schema = await buildSchema({
     resolvers: [BookResolver, AuthorResolver],
@@ -19,25 +18,35 @@ async function bootstrap() {
 
   const server = new ApolloServer({
     schema,
-    context: {},
-    onHealthCheck: () => {
-      return new Promise((resolve, reject) => {
-        // Replace the `true` in this conditional with more specific checks!
-        // URL: http://localhost:4000/.well-known/apollo/server-health
-
-        if (true) {
-          resolve('ok');
-        } else {
-          reject();
-        }
-      });
+    context: {
+      session: {},
     },
   });
 
-  // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  await server.start();
+  const app = express();
+
+  app.get('/health', async (req, res) => {
+    try {
+      if (dbConnection.isConnected) return res.send(true);
+      return res.send(false);
+    } catch (e) {
+      console.error(e);
+      return res.send(false);
+    }
   });
+
+  server.applyMiddleware({
+    app,
+    path: '/',
+  });
+
+  await new Promise((resolve) =>
+    app.listen({ port: PORT }, () => {
+      resolve('ok');
+      console.log(`🚀  Server ready at port=>${PORT}`);
+    })
+  );
 }
 
 bootstrap();
